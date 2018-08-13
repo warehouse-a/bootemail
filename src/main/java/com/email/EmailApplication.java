bdd6ff7f40a36173ddbadcfac21c58cdeca040c8
wk.li <13722348598@163.com>
2018-08-13 20:42:17 +0800

    123

--- src/main/java/com/email/EmailApplication.java
+++ src/main/java/com/email/EmailApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EmailApplication {
 
+	public void test(){
+		
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(EmailApplication.class, args);
 	}
